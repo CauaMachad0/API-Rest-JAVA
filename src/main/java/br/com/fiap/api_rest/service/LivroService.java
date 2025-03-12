b9031595b35b0915c70e1c0fb86d2027b2909f6c
CauaMachad0 <caua.marcelo.machado@outlook.com>
2025-03-11 22:50:17 -0300

    Aprendendo uso de links

--- src/main/java/br/com/fiap/api_rest/service/LivroService.java
+++ src/main/java/br/com/fiap/api_rest/service/LivroService.java
@@ -1,17 +1,24 @@
+
 package br.com.fiap.api_rest.service;
 
+import br.com.fiap.api_rest.controller.LivroController;
 import br.com.fiap.api_rest.dto.LivroRequest;
 import br.com.fiap.api_rest.dto.LivroRequestDTO;
 import br.com.fiap.api_rest.dto.LivroResponse;
+import br.com.fiap.api_rest.dto.LivroResponseDTO;
 import br.com.fiap.api_rest.model.Livro;
 import br.com.fiap.api_rest.repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.Link;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
 public class LivroService {
@@ -39,6 +46,16 @@ public class LivroService {
         return new LivroResponse(livro.getId(), livro.getAutor() + " - " + livro.getTitulo());
     }
 
+    public LivroResponseDTO livroToResponseDTO(Livro livro, boolean self) {
+        Link link;
+        if (self) {
+            link = linkTo(methodOn(LivroController.class).readLivro(livro.getId())).withSelfRel();
+        } else {
+            link = linkTo(methodOn(LivroController.class).readLivros(0)).withRel("Lista de Livros");
+        }
+        return new LivroResponseDTO(livro.getId(), livro.getAutor() + " - " + livro.getTitulo(), link);
+    }
+
     public List<LivroResponse> livrosToResponse(List<Livro> livros) {
         List<LivroResponse> listaLivros = new ArrayList<>();
         for (Livro livro : livros) {
@@ -50,5 +67,9 @@ public class LivroService {
     public Page<LivroResponse> findAll(Pageable pageable) {
         //return livroRepository.findAll(pageable).map(livro -> livroToResponse(livro));
         return livroRepository.findAll(pageable).map(this::livroToResponse);
+    }
+
+    public Page<LivroResponseDTO> findAllDTO(Pageable pageable) {
+        return livroRepository.findAll(pageable).map(livro -> livroToResponseDTO(livro, true));
     }
 }
